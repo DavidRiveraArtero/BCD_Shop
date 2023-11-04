99cf2773140764d4d95dd53bf2415c08775612bf
David Rivera <davidtoy222@gmail.com>
2023-11-04 18:44:13 +0100

    responsive version phone

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -7,7 +7,7 @@ export function Home (){
             <InputP />
             <Link to="/element"  className="sectionGamer rounded-lg m-4 pb-12 pt-12 bg-gradient-to-b from-black via-neutral-600 to-slate-300 flex flex-row items-center">
                 <div className="box_img ml-4"/>
-                <h2 className=" absolute max-sm:text-3xl  max-sm:w-[200px] ml-[140px]">Lo mejor para los Gamers</h2>
+                <h2 className=" absolute text-3xl  max-sm:w-[200px] ml-[140px]">Lo mejor para los Gamers</h2>
             </Link>
           
         </>
